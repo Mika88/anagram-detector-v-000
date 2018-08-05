204d9a80d59641a50a69bc3db8384f5749f6fab3
Mika88 <mikaspivak@gmail.com>
2018-08-05 15:37:28 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,14 @@ class Anagram
   def initialize(word)
     @word = word
   end
+  def match(array_words)
+    anagram_array = []
+    array_words.each do |word_a|
+      if word_a.split(" ").sort == word.split(" ").sort
+        anagram_array << word_a
+      else
+        anagram_array
+      end
+    end
+  end
 end
