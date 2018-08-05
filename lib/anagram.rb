f4e7707ef790ee04971937a8cd61fea0ccf86a87
Mika88 <mikaspivak@gmail.com>
2018-08-05 16:15:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,13 @@ class Anagram
     @word = word
   end
   def match(array_words)
+  
          array_words.map do |word_a|
-         word_a.scan(/\w/).sort == word.scan(/\w/).sort
+          if word_a.scan(/\w/).sort == word.scan(/\w/).sort
+            word_a
+         else
+          []
+         end
        end
   end
 end
